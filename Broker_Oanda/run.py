c90a99516d748f86f24bc25b39168267b7691b35
matgc <mateusgc@gmail.com>
2025-08-15 08:19:00 -0300

    v1.02

--- Broker_Oanda/run.py
+++ Broker_Oanda/run.py
@@ -5,9 +5,9 @@ from api import OandaApi
 if __name__ == '__main__':
     api = OandaApi()
 
-    symbol_lst      = ['EUR_USD']
-    granularity_lst = ['H1']
-    date_start      = '2025-03-26T00:00:00'
-    date_end        = '2025-03-28T00:00:00'
+    symbol_lst      = ['EUR_USD']           #Change as needed
+    granularity_lst = ['H1']                #Change as needed
+    date_start      = '2025-03-26T00:00:00' #Change as needed
+    date_end        = '2025-03-28T00:00:00' #Change as needed
 
     get_hist_quotes(symbol_lst, granularity_lst, date_start, date_end, api)
