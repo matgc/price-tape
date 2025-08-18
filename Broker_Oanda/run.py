54acc89b7f81e494d6eed9a51fcbf7bc157acdc9
Mateus Rapini <mateusrapini@Mac.localdomain>
2025-08-18 13:18:03 -0300

    v1.05 - added jupyter notebook's version of the code

--- Broker_Oanda/run.py
+++ Broker_Oanda/run.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     api = OandaApi()
 
     symbol_lst      = ['EUR_USD']           
-    granularity_lst = ['D','H4', 'H2', 'H1', 'M30', 'M15', 'M5', 'M1']      
-    date_start      = '2000-01-01T00:00:00' 
-    date_end        = '2025-08-01T00:00:00' 
+    granularity_lst = ['H1']
+    date_start      = '2025-03-25T00:00:00'
+    date_end        = '2025-03-29T00:00:00'
 
     get_hist_quotes(symbol_lst, granularity_lst, date_start, date_end, api)
