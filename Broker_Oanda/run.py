7fa9bfd6c4896aa9f6f8520e063a9d2bf7b247c4
Mateus Rapini <mateusrapini@Mac.localdomain>
2025-09-08 17:32:51 -0300

    Correct excel conversion

--- Broker_Oanda/run.py
+++ Broker_Oanda/run.py
@@ -5,9 +5,9 @@ from api import OandaApi
 if __name__ == '__main__':
     api = OandaApi()
 
-    symbol_lst      = ['EUR_USD']           
-    granularity_lst = ['H1']
+    symbol_lst      = ['USD_JPY']           
+    granularity_lst = ['M1']
     date_start      = '2025-03-25T00:00:00'
-    date_end        = '2025-03-29T00:00:00'
+    date_end        = '2025-03-26T00:00:00'
 
     get_hist_quotes(symbol_lst, granularity_lst, date_start, date_end, api)
